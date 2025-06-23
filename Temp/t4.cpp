ee49932025d5c265afa14d09c96d74e495824818
YashVS15112004 <114746056+YashVS15112004@users.noreply.github.com>
2025-06-23 15:50:50 +0530

    CPP_DSA

--- Temp/t4.cpp
+++ Temp/t4.cpp
@@ -1,41 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool cmp(vector<int> &a,vector<int> &b) 
-{ 
-    return  a[1]< b[1]; 
-} 
-vector<int> frequencySort(vector<int>& nums)
+int digit[100];
+long long kMirror(int k, int n)
 {
-    if(nums.size()<=1) return nums;
-    vector<int> ans;
-    map<int,int> mp;
-    for(auto it:nums) mp[it]++;
-    vector<vector<int>> vec;
-    for(auto it:mp)
+    auto isPalindrome = [&](long long x) -> bool
     {
-        vector<int> temp;
-        temp.push_back(it.first);
-        temp.push_back(it.second);
-        vec.push_back(temp);
-    }
-    sort(vec.begin(),vec.end(),cmp);
-    for(int i=0;i<vec.size();i++)
-    ans.push_back(vec[i][0]);
-    nums.clear();
-    for(int i=0;i<ans.size();i++)
-    {
-        int k = mp[ans[i]];
-        for(int j=0;j<k;+j++)
+        int length = -1;
+        while (x)
         {
-            nums.push_back(ans[i]);
+            ++length;
+            digit[length] = x % k;
+            x /= k;
         }
+        for (int i = 0, j = length; i < j; ++i, --j)
+        {
+            if (digit[i] != digit[j])
+            {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    int left = 1, count = 0;
+    long long ans = 0;
+    while (count < n)
+    {
+        int right = left * 10;
+        // op = 0 indicates enumerating odd-length palindromes
+        // op = 1 indicates enumerating even-length palindromes
+        for (int op = 0; op < 2; ++op)
+        {
+            // enumerate i'
+            for (int i = left; i < right && count < n; ++i)
+            {
+                long long combined = i;
+                int x = (op == 0 ? i / 10 : i);
+                while (x)
+                {
+                    combined = combined * 10 + x % 10;
+                    x /= 10;
+                }
+                if (isPalindrome(combined))
+                {
+                    ++count;
+                    ans += combined;
+                }
+            }
+        }
+        left = right;
     }
-    //sort(nums.begin(),nums.end(),greater<int>());
-    return nums;
+    return ans;
 }
 int main()
 {
-    vector<int> nums = {-1,1,-6,4,5,-6,1,4,1};
-    vector<int> ans = frequencySort(nums);
-    for(auto it:ans) cout<<it<<" ";
+    cout << kMirror(2, 5);
 }
